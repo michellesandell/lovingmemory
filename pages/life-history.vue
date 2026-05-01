832f69bcfe64f3c536bbbbd25343d8bf4160bf73
Michelle <234535200+michellesandell@users.noreply.github.com>
2026-05-02 00:29:23 +1000

    Update life-history.vue

--- pages/life-history.vue
+++ pages/life-history.vue
@@ -6,6 +6,8 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Michelle's Life</h1>
+      In 1955 my parents and my sister drove across the Nullabor Plain to Western Australia in their blue Morris Minor four door sedan towing a trailer packed with all their worldly possessions.
+      
       <!-- CONTENT END -->
     </div>
   </div>
