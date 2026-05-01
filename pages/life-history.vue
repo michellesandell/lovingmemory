09aa3df67e3bd8888fef957773979ff675eed988
Michelle <234535200+michellesandell@users.noreply.github.com>
2026-05-01 22:17:55 +1000

    Update files

--- pages/life-history.vue
+++ pages/life-history.vue
@@ -5,19 +5,7 @@
     </div>
     <div class="section details">
       <!-- CONTENT START -->
-      <h1 class="title">Service Livestream</h1>
-      <p>As attendance is restricted due to COVID-19 we hope that you will join us virtually. The services will be held on <em>Thursday January 28th 2021</em>, first at Church address then at Crematorium address.</p>
-      <h2>Church Service</h2>
-      <p>The church service is being performed at Church address
-      </p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 10:30am</a>
-      <h2>Crematorium</h2>
-      <p>The service is being held at Crematorium address</p>
-      <a class="button" target="_blank" rel=”noopener” href="">Livestream - 12:00pm</a>
-      <ul class="fa-ul">
-        <ListItem>Username: <span class="highlight">test123</span> <CopyButton text="test123"></CopyButton></ListItem>
-        <ListItem>Password: <span class="highlight">password123</span> <CopyButton text="password123"></CopyButton></ListItem>
-      </ul>
+      <h1 class="title">Michelle's Life</h1>
       <!-- CONTENT END -->
     </div>
   </div>
